8dda6c08d429a0282f328a0e188162073fbe509a
Laura Slor <slorlaura@gmail.com>
2017-10-11 03:18:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidays(parameter) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
